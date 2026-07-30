e8c954ca56ad3df1f089a9da85d3b40e4cb85725
zan-afn <zanfn@example.com>
2026-07-30 17:35:22 +0700

    Initial commit create spring boot project

---
+++ src/test/java/com/kost/kostapi/KostApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kost.kostapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KostApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
